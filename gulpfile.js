42831d3766b3230edbe6df98006c92f77c2b93e8
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2016-09-22 16:06:18 +0200

    Implement incremental builds on gulp watch
    Move dependencies to dev dependencies
    Add atom ctags to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
+/* eslint-disable quotes */
+
 var gulp     = require('gulp'),
   autoprefix = require('gulp-autoprefixer'),
   livereload = require('gulp-livereload'),
   sourcemaps = require('gulp-sourcemaps'),
+  remember   = require('gulp-remember'),
   plumber    = require('gulp-plumber'),
   cssnano    = require('gulp-cssnano'),
+  cached     = require('gulp-cached'),
   notify     = require('gulp-notify'),
   concat     = require('gulp-concat'),
   rename     = require('gulp-rename'),
@@ -23,16 +27,18 @@ function handleErrors() {
 }
 
 gulp.task('sass', function() {
-  return gulp.src('./assets/stylesheets/main.scss')
+  return gulp.src('assets/stylesheets/main.scss')
     .pipe(plumber({ errorHandler: handleErrors }))
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(cached('sass'))
+    .pipe(sass({ includePaths: ['./node_modules/bulma'] }))
     .pipe(autoprefix({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(cssnano())
-    .pipe(sourcemaps.write("."))
-    .pipe(rename('style.min.css'))
+    .pipe(remember('sass'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename('stylesheet.min.css'))
     .pipe(gulp.dest('public/assets/stylesheets/'))
     .pipe(livereload());
 });
@@ -41,18 +47,23 @@ gulp.task('js', function() {
   return gulp.src('assets/javascripts/**/*')
     .pipe(plumber({ errorHandler: handleErrors }))
     .pipe(sourcemaps.init())
+    .pipe(cached('js'))
     .pipe(babel())
+    .pipe(remember('scripts'))
     .pipe(concat('javascript.js'))
     .pipe(uglify())
-    .pipe(sourcemaps.write("."))
+    .pipe(sourcemaps.write('.'))
     .pipe(rename('javascript.min.js'))
-    .pipe(gulp.dest('public/assets/javascripts/'))
+    .pipe(gulp.dest('public/assets/javascripts/'));
 });
 
-// create a default task and just log a message
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('assets/sass/**/*', ['sass']);
+  gulp.watch('assets/stylesheets/**/*', ['sass']);
   gulp.watch('assets/javascripts/**/*', ['js']);
   gutil.log('Watching all .scss and js files...');
 });
+
+gulp.task('default', ['sass', 'js']);
+
+/* eslint-enable quotes */
