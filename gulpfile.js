5f32650771b132a1754b4aaf149b0a1d1b39591f
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2016-09-29 15:47:44 +0200

    Switch from eslint to standard

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-/* eslint-disable quotes, no-undef */
-'use strict';
+'use strict'
 
 /**
 * gulpfile.js
@@ -25,60 +24,57 @@
 *
 * Modules:
 *   gulp                       : The streaming build system.
-*   del                        : Delete files and folders.
 *   child_process              : Spawn child process through Node.
+*   del                        : Delete files and folders.
 *   gulp-autoprefixer          : Prefix CSS.
 *   gulp-babel                 : Transpile ES6+ js files.
-*   gulp-concat                : Concatenates files.
 *   gulp-cleancss              : Minify CSS with clean-css.
+*   gulp-concat                : Concatenates files.
 *   gulp-csscomb               : Format and sort SCSS/CSS files with CSScomb
-*   gulp-eslint                : Lint JS files with ESLint
-*   gulp-livereload            : Reload web browser on demand
 *   gulp-jsbeautifier          : Prettify JavaScript, JSON, HTML and CSS.
+*   gulp-livereload            : Reload web browser on demand
 *   gulp-notify                : Notify system.
 *   gulp-plumber               : Prevent pipe breaking caused by errors.
 *   gulp-rename                : Rename files.
 *   gulp-sass                  : Gulp plugin for sass.
-*   gulp-scsslint              : Lint SCSS files with scss-lint.
 *   gulp-sourcemaps            : Source map support for Gulp.js.
 *   gulp-sync                  : Sync Gulp tasks.
 *   gulp-uglify                : Minify files with UglifyJS.
 *   gulp-util                  : Utility functions for gulp plugins.
 */
 
-var gulp     = require('gulp'),
-  autoprefix = require('gulp-autoprefixer'),
-  livereload = require('gulp-livereload'),
-  sourcemaps = require('gulp-sourcemaps'),
-  prettify   = require('gulp-jsbeautifier'),
-  cleancss   = require('gulp-clean-css'),
-  scsslint   = require('gulp-scss-lint'),
-  csscomb    = require('gulp-csscomb'),
-  plumber    = require('gulp-plumber'),
-  gulpif     = require('gulp-if'),
-  eslint     = require('gulp-eslint'),
-  notify     = require('gulp-notify'),
-  concat     = require('gulp-concat'),
-  rename     = require('gulp-rename'),
-  uglify     = require('gulp-uglify'),
-  gutil      = require('gulp-util'),
-  babel      = require('gulp-babel'),
-  child      = require('child_process'),
-  sync       = require('gulp-sync')(gulp),
-  sass       = require('gulp-sass'),
-  args       = require('yargs').argv,
-  del        = require('del');
+var args = require('yargs').argv
+var autoprefix = require('gulp-autoprefixer')
+var babel = require('gulp-babel')
+var child = require('child_process')
+var cleancss = require('gulp-clean-css')
+var concat = require('gulp-concat')
+var csscomb = require('gulp-csscomb')
+var del = require('del')
+var gulp = require('gulp')
+var gulpif = require('gulp-if')
+var gutil = require('gulp-util')
+var livereload = require('gulp-livereload')
+var notify = require('gulp-notify')
+var plumber = require('gulp-plumber')
+var rename = require('gulp-rename')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
+var standard = require('gulp-standard')
+var standardFormat = require('gulp-standard-format')
+var sync = require('gulp-sync')(gulp)
+var uglify = require('gulp-uglify')
 
 /* ----------------------------------------------------------------------------
  * locals
  * ------------------------------------------------------------------------- */
 
-var server = null;
+var server = null
 
 var root = {
   src: './assets/',
   dest: './public/assets/'
-};
+}
 
 var css = {
   src: 'stylesheets/',
@@ -88,22 +84,22 @@ var css = {
   include: [
     './node_modules/bulma'
   ]
-};
+}
 
 var js = {
   src: 'javascripts/',
   dest: 'javascripts/',
   watch: '**/*'
-};
+}
 
 var views = {
   src: 'views/',
   watch: '**/*.tmpl'
-};
+}
 
 var go = {
   src: '*/**/*.go'
-};
+}
 
 /* ----------------------------------------------------------------------------
  * Functions
@@ -115,15 +111,15 @@ var go = {
  * handleErrors                   : Send a notification to the system on error.
  */
 
-function handleErrors() {
-  var args = Array.prototype.slice.call(arguments);
+function handleErrors () {
+  var args = Array.prototype.slice.call(arguments)
 
   notify.onError({
     title: '<%= error.plugin %> failed',
     message: '<%= error.message %>'
-  }).apply(this, args);
-  gutil.beep();
-  this.emit('end');
+  }).apply(this, args)
+  gutil.beep()
+  this.emit('end')
 }
 
 /* ----------------------------------------------------------------------------
@@ -138,7 +134,7 @@ function handleErrors() {
 * assets:stylesheets:clean       : Remove any CSS files in stylesheet dist folder.
 */
 
-gulp.task('assets:stylesheets', function() {
+gulp.task('assets:stylesheets', function () {
   return gulp.src(root.src + css.src + css.main)
     .pipe(plumber({ errorHandler: handleErrors }))
     .pipe(gulpif(args.dev, sourcemaps.init()))
@@ -150,19 +146,19 @@ gulp.task('assets:stylesheets', function() {
     .pipe(rename('stylesheet.min.css'))
     .pipe(gulpif(args.dev, sourcemaps.write()))
     .pipe(gulp.dest(root.dest + css.dest))
-    .pipe(gulpif(args.dev, livereload()));
-});
+    .pipe(gulpif(args.dev, livereload()))
+})
 
-gulp.task('assets:stylesheets:beautify', function() {
+gulp.task('assets:stylesheets:beautify', function () {
   return gulp.src(root.src + css.src + css.watch)
    .pipe(plumber({ errorHandler: handleErrors }))
    .pipe(csscomb())
-   .pipe(gulp.dest(root.src + css.src));
-});
+   .pipe(gulp.dest(root.src + css.src))
+})
 
-gulp.task('assets:stylesheets:clean', function() {
-  return del([root.dest + css.dest]);
-});
+gulp.task('assets:stylesheets:clean', function () {
+  return del([root.dest + css.dest])
+})
 
 /**
 * JS Tasks
@@ -171,31 +167,32 @@ gulp.task('assets:stylesheets:clean', function() {
 * assets:javascripts:beautify    : Beautify JS files.
 * assets:javascripts:clean       : Remove any JS files in javascript dist folder.
 */
-gulp.task('assets:javascripts', function() {
+gulp.task('assets:javascripts', function () {
   return gulp.src(root.src + js.src + js.watch)
     .pipe(plumber({ errorHandler: handleErrors }))
     .pipe(gulpif(args.dev, sourcemaps.init()))
-    .pipe(prettify())
-    .pipe(eslint())
-    .pipe(babel())
+    .pipe(standardFormat())
+    .pipe(standard())
+    .pipe(babel({ presets: ['es2015'] }))
     .pipe(concat('javascript.js'))
     .pipe(uglify())
     .pipe(rename('javascript.min.js'))
     .pipe(gulpif(args.dev, sourcemaps.write()))
     .pipe(gulp.dest(root.dest + js.dest))
-    .pipe(gulpif(args.dev, livereload()));
-});
+    .pipe(gulpif(args.dev, livereload()))
+})
 
-gulp.task('assets:javascripts:beautify', function() {
+gulp.task('assets:javascripts:beautify', function () {
   return gulp.src(root.src + js.src + js.watch)
    .pipe(plumber({ errorHandler: handleErrors }))
-   .pipe(prettify())
-   .pipe(gulp.dest(root.src + js.src));
-});
+   .pipe(standardFormat())
+   .pipe(standard())
+   .pipe(gulp.dest(root.src + js.src))
+})
 
-gulp.task('assets:javascripts:clean', function() {
-  return del([root.dest + js.dest]);
-});
+gulp.task('assets:javascripts:clean', function () {
+  return del([root.dest + js.dest])
+})
 
 /**
  * Global assets tasks
@@ -205,23 +202,23 @@ gulp.task('assets:javascripts:clean', function() {
  * assets:build                   : Clean/beautify/compile CSS/JS files.
  */
 
-gulp.task('assets:watch', function() {
-  gulp.watch([root.src + css.src + css.watch], ['assets:stylesheets']);
-  gulp.watch([root.src + js.src + js.watch], ['assets:javascripts']);
-});
+gulp.task('assets:watch', function () {
+  gulp.watch([root.src + css.src + css.watch], ['assets:stylesheets'])
+  gulp.watch([root.src + js.src + js.watch], ['assets:javascripts'])
+})
 
 gulp.task('assets:clean', [
   'assets:stylesheets:clean',
   'assets:javascripts:clean'
-]);
+])
 
 gulp.task('assets:build', [
   'assets:clean',
   'assets:stylesheets:beautify',
   'assets:stylesheets',
   'assets:javascripts:beautify',
-  'assets:javascripts',
-]);
+  'assets:javascripts'
+])
 
 /* ----------------------------------------------------------------------------
  * Server
@@ -235,50 +232,50 @@ gulp.task('assets:build', [
 * server:watch                   : Watch Go/tmpl files for changes and restart server.
 */
 
-gulp.task('server:build', function() {
-  var build = child.spawnSync('go', ['install']);
+gulp.task('server:build', function () {
+  var build = child.spawnSync('go', ['install'])
 
   // Log build errors
   if (build.stderr.length) {
     var lines = build.stderr
       .toString()
       .split('\n')
-      .filter(function(line) {
-        return line.length;
-      });
+      .filter(function (line) {
+        return line.length
+      })
 
     for (var l in lines) {
       gutil.log(
         gutil.colors.red('Error (go install): ' + lines[l])
-      );
+      )
     }
 
     notify.notify({
       title: 'Error (go install)',
       message: lines
-    });
+    })
   }
 
-  return build;
-});
+  return build
+})
 
-gulp.task('server:spawn', function() {
+gulp.task('server:spawn', function () {
   if (server) {
-    server.kill();
+    server.kill()
   }
 
-  server = child.spawn('go', ['run', 'breizhtrip.go', 'server', '-p', '5050'], { stdio: 'inherit' });
+  server = child.spawn('go', ['run', 'breizhtrip.go', 'server', '-p', '5050'], { stdio: 'inherit' })
 
-  livereload();
-});
+  livereload()
+})
 
-gulp.task('server:watch', function() {
-  gulp.watch([views.src + views.watch], ['server:spawn']);
+gulp.task('server:watch', function () {
+  gulp.watch([views.src + views.watch], ['server:spawn'])
   gulp.watch(go.src, sync.sync([
     'server:build',
     'server:spawn'
-  ]));
-});
+  ]))
+})
 
 /* ----------------------------------------------------------------------------
  * Interface
@@ -294,15 +291,13 @@ gulp.task('server:watch', function() {
 gulp.task('build', [
   'assets:build',
   'server:build'
-]);
+])
 
-gulp.task('watch', ['build'], function() {
-  livereload.listen();
+gulp.task('watch', ['build'], function () {
+  livereload.listen()
   return gulp.start([
     'assets:watch',
     'server:watch',
     'server:spawn'
-  ]);
-});
-
-/* eslint-enable quotes, no-undef */
+  ])
+})
