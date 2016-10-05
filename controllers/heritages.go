387b9e038024b68962c3a1786044dfb78d1df8d6
mdouchement <marc.douchement@predicsis.com>
2016-10-05 23:30:31 +0200

    initialized filtering

--- controllers/heritages.go
+++ controllers/heritages.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"github.com/mdouchement/breizhtrip-go/config"
 	"github.com/mdouchement/breizhtrip-go/models"
 )
@@ -29,4 +31,23 @@ func (h *Heritages) Show(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, heritage)
+}
+
+//---------------------//
+// Filters             //
+//---------------------//
+// https://github.com/mdouchement/breizhtrip/blob/master/app/contexts/legacies_view.rb
+
+type filter gorm.DB
+
+func (f *filter) like(params map[string]interface{}) {
+	for k, v := range params {
+		f = f.Where(fmt.Sprintf("%s ILIKE '%%%s%'", k, v))
+	}
+}
+
+func (f *filter) sort(params map[string]interface{}) {
+}
+
+func (f *filter) area(longitude, latitude float64) {
 }
